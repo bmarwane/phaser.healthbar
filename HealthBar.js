02d39b923ff40fed4743122500be00b7d7362c64
Marwane <mbelahcen@octo.com>
2015-08-31 09:00:20 +0100

    put HealthBar.js in the root

---
+++ HealthBar.js
@@ -0,0 +1,128 @@
+/**
+   Copyright (c) 2015 Belahcen Marwane (b.marwane@gmail.com)
+
+   Permission is hereby granted, free of charge, to any person obtaining a copy
+   of this software and associated documentation files (the "Software"), to deal
+   in the Software without restriction, including without limitation the rights
+   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+   copies of the Software, and to permit persons to whom the Software is
+   furnished to do so, subject to the following conditions:
+
+   The above copyright notice and this permission notice shall be included in all
+   copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+   SOFTWARE.
+ */
+
+var HealthBar = function(game, providedConfig) {
+  this.game = game;
+
+  this.setupConfiguration(providedConfig);
+  this.setPosition(this.config.x, this.config.y);
+  this.drawBackground();
+  this.drawHealthBar();
+};
+HealthBar.prototype.constructor = HealthBar;
+module.exports = HealthBar;
+
+HealthBar.prototype.setupConfiguration = function (providedConfig) {
+  this.config = this.mergeWithDefaultConfiguration(providedConfig);
+  this.flipped = this.config.flipped;
+};
+
+HealthBar.prototype.mergeWithDefaultConfiguration = function(newConfig) {
+  var defaultConfig= {
+    width: 250,
+    height: 40,
+    x: 0,
+    y: 0,
+    bg: {
+      color: '#651828'
+    },
+    bar: {
+      color: '#FEFF03'
+    },
+    animationDuration: 200,
+    flipped: false
+  };
+
+  return mergeObjetcs(defaultConfig, newConfig);
+};
+
+function mergeObjetcs(targetObj, newObj) {
+  for (var p in newObj) {
+    try {
+      targetObj[p] = newObj[p].constructor==Object ? mergeObjetcs(targetObj[p], newObj[p]) : newObj[p];
+    } catch(e) {
+      targetObj[p] = newObj[p];
+    }
+  }
+  return targetObj;
+}
+
+HealthBar.prototype.drawBackground = function() {
+
+  var bmd = this.game.add.bitmapData(this.config.width, this.config.height);
+  bmd.ctx.fillStyle = this.config.bg.color;
+  bmd.ctx.beginPath();
+  bmd.ctx.rect(0, 0, this.config.width, this.config.height);
+  bmd.ctx.fill();
+
+  this.bgSprite = this.game.add.sprite(this.x, this.y, bmd);
+  this.bgSprite.anchor.set(0.5);
+
+  if(this.flipped){
+    this.bgSprite.scale.x = -1;
+  }
+};
+
+HealthBar.prototype.drawHealthBar = function() {
+  var bmd = this.game.add.bitmapData(this.config.width, this.config.height);
+  bmd.ctx.fillStyle = this.config.bar.color;
+  bmd.ctx.beginPath();
+  bmd.ctx.rect(0, 0, this.config.width, this.config.height);
+  bmd.ctx.fill();
+
+  this.barSprite = this.game.add.sprite(this.x - this.bgSprite.width/2, this.y, bmd);
+  this.barSprite.anchor.y = 0.5;
+
+  if(this.flipped){
+    this.barSprite.scale.x = -1;
+  }
+};
+
+HealthBar.prototype.setPosition = function (x, y) {
+  this.x = x;
+  this.y = y;
+
+  if(this.bgSprite !== undefined & this.barSprite !== undefined){
+    this.bgSprite.position.x = x;
+    this.bgSprite.position.y = y;
+
+    this.barSprite.position.x = x - this.config.width/2;
+    this.barSprite.position.y = y;
+  }
+};
+
+
+HealthBar.prototype.setPercent = function(newValue){
+  if(newValue < 0) newValue = 0;
+  if(newValue > 100) newValue = 100;
+
+  var newWidth =  (newValue * this.config.width) / 100;
+
+  this.setWidth(newWidth);
+};
+
+HealthBar.prototype.setWidth = function(newWidth){
+  if(this.flipped) {
+    newWidth = -1 * newWidth;
+  }
+  this.game.add.tween(this.barSprite).to( { width: newWidth }, this.config.animationDuration, Phaser.Easing.Linear.None, true);
+};
